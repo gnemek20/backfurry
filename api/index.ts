d6dce13a046df80308ed8b502fba837eabae3aec
gnemek20 <gnemek20@gmail.com>
2024-05-26 15:28:16 +0900

    Fur!Ry

--- api/index.ts
+++ api/index.ts
@@ -2,12 +2,16 @@ var express = require('express');
 var app = express();
 
 // memory setup
-app.use(express.json({ limit: '100mb' }));
-app.use(express.urlencoded({ limit: '100mb', extended: true }));
+app.use(express.json({ limit: '1000mb' }));
+app.use(express.urlencoded({ limit: '1000mb', extended: true }));
 
 // parser setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+// cors setup
+const cors = require('cors');
+app.use(cors());
 
 // storage setup
 const multer = require('multer');
